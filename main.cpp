8fdbe100ba91d07c77bee62a66f564c64f30724f
snuczek <26097711+snuk0@users.noreply.github.com>
2020-05-25 12:40:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,24 +7,29 @@ int main()
 {
 	int liczby;
 	fstream plik;  //deklaracja zmiennej pliku tekstowego
-
-	plik.open("plik.txt", ios::in);  //otwarcie pliku
-
+	queue < int > kolejkaLiczb;
+	plik.open("D:\\Programy\\C++\\Kolejka\\plik.txt", ios::in);  //otwarcie pliku
+	if (plik.good())  //sprawdzenie czy plik istnieje
+		while (!plik.eof())  //petla wykonuje sie az program dojedzie do konca pliku
+		{
+			plik >> liczby;  //pobranie linii do zmiennej napis
+			
+			kolejkaLiczb.push(liczby);
+			
+		}
+	plik.close();  //zamkniecie pliku tekstowego
 
 	cout << "ContentType:application/json;charset=UTF-8" << (char)13 << (char)10
 		<< "\n"
 		<< "[ \n ";
-		if (plik.good())  //sprawdzenie czy plik istnieje
-			while (!plik.eof())  //petla wykonuje sie az program dojedzie do konca pliku
-			{
-				plik >> liczby;  //pobranie linii do zmiennej napis
-				queue < int > kolejkaLiczb;
-				kolejkaLiczb.push(liczby);
-				cout << "  { \"liczba\":\" " << liczby << "\" }, \n";
-
-				cout << endl;
-			}
-		plik.close();  //zamkniecie pliku tekstowego
+	while (!kolejkaLiczb.empty()) {
+		cout << "  { \"liczba\": \"" << kolejkaLiczb.front() << "\" }";
+		if (kolejkaLiczb.size() != 1)
+			cout << ",";
+		cout << "\n";
+		kolejkaLiczb.pop();
+	}
+	
 	cout << "]";
 
 	return 0;
